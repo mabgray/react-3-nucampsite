641d5bca0af0c678bd10dd3eff8bcbc1a3195f7a
Maryanne Gray <m2ry2nnegr2y@gmail.com>
2021-12-22 13:16:23 -0600

    breadcrumbs

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,6 +1,7 @@
 
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
 
@@ -13,7 +14,7 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
                  <Card>
                     <CardImg top src={campsite.image} alt={campsite.name} />
                     <CardBody>
-                        <CardTitle>{campsite.name}</CardTitle>
+                     
                         <CardText>{campsite.description}</CardText>
                     </CardBody>
                 </Card>
@@ -52,12 +53,20 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
     function CampsiteInfo(props){
         if(props.campsite){
             return (
-            <div className="container">
+                <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to="/directory">Directory</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>{props.campsite.name}</BreadcrumbItem>
+                        </Breadcrumb>
+                        <h2>{props.campsite.name}</h2>
+                        <hr />
+                    </div>
+                </div>
                 <div className="row">
                     <RenderCampsite campsite={props.campsite} />
-                    {/* <RenderComments commnets={props.campsite.comments} /> */}
                     <RenderComments comments={props.comments} />
-                    
                 </div>
             </div>
             )
