718f265c3f88d3f47f85d7c3e3257212f6a26753
Maryanne Gray <m2ry2nnegr2y@gmail.com>
2022-01-04 14:41:24 -0600

    redux thunk

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -4,6 +4,7 @@ import React, { Component }  from 'react';
 import { Modal, ModalHeader, ModalBody,Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem, Button, Label } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
 
 
 const required = val => val && val.length;
@@ -174,6 +175,32 @@ function RenderComments({comments, addComment, campsiteId}){
 
 
     function CampsiteInfo(props){
+
+
+        if (props.isLoading) {
+            return (
+                <div className="container">
+                    <div className="row">
+                        <Loading />
+                    </div>
+                </div>
+            );
+        }
+        if (props.errMess) {
+            return (
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            <h4>{props.errMess}</h4>
+                        </div>
+                    </div>
+                </div>
+            );
+        }
+
+
+        
+        
         if(props.campsite){
             return (
                 <div className="container">
